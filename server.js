d10d70e45e79ec7c401a0089b7e9c894a9dc3935
cliffkoome <koomekoome902@gmail.com>
2025-09-28 15:07:47 +0300

    Implement user registration and login functionality in server.js; add index.html file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 app.use(express.json())
 
@@ -16,6 +17,39 @@ const posts = [
     title: "Post 2",
   },
 ];
+
+const users = []
+
+app.get('/users', (req, res) => {
+  res.json(users);
+})
+
+app.post('/users', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
+  } catch  {
+    res.status(500).send();
+  }
+})
+
+app.post('/users/login', async (req, res) => {
+  const user = users.find(user => user.name = req.body.name)
+  if (user == null) {
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success')
+    } else {
+      res.send('Not Allowed')
+    }
+  } catch {
+    res.status(500).send();
+  }
+})
 
 app.get('/posts', authenticateToken, (req, res) => {
   res.json(posts.filter(post => post.username === req.user.name))
